22abbc8726dae5896c11a01d1c3f1ded813c29c3
bbogeul <illumeweb@gmail.com>
2020-02-07 05:16:30 +0900

    user find my page

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,10 +1,14 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get } from '@nestjs/common';
 import { User } from './user.entity';
+import { UserInfo } from '../../common';
 import { BaseController } from '../../core';
 import { UserService } from './user.service';
 import { UserCreateDto } from './dto';
+import { ApiTags, ApiBearerAuth } from '@nestjs/swagger';
 
 @Controller()
+@ApiTags('User', 'USER')
+@ApiBearerAuth()
 export class UserController extends BaseController {
   constructor(private readonly userService: UserService) {
     super();
@@ -17,5 +21,14 @@ export class UserController extends BaseController {
   @Post('/user')
   async create(@Body() userCreateDto: UserCreateDto): Promise<User> {
     return await this.userService.create(userCreateDto);
+  }
+
+  /**
+   * 본인 페이지 가져오기
+   * @param user
+   */
+  @Get('/my-page')
+  async findMe(@UserInfo() user: User): Promise<User> {
+    return await this.userService.findOne(user.id);
   }
 }
--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -5,8 +5,9 @@ import { UserController } from './user.controller';
 import { AdminUserController } from './admin-user.controller';
 import { UserService } from './user.service';
 import { PasswordService } from '../auth/password.service';
+import { UserHistory } from './user-history.entity';
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, UserHistory])],
   controllers: [UserController, AdminUserController],
   providers: [UserService, PasswordService],
 })
